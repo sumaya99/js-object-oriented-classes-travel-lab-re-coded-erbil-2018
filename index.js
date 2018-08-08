a4fec87af2e2b027baa494a53af0e59365ffa59f
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-08 06:39:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ class Driver{
   Driver.prototype.yearsExperienceFromBeginningOf(){}
 }
 let driver = new Driver("Alfie", "Aug 9, 1995")
+
+class Route extends Driver{
+  constructor(name, thedayjoined ){
+  this.name=name
+  this.thedayjoined=thedayjoined
+  }
+  Driver.prototype.startDate(){}
+  Driver.prototype.yearsExperienceFromBeginningOf(){}
+}
+let driver = new Driver("Alfie", "Aug 9, 1995")
