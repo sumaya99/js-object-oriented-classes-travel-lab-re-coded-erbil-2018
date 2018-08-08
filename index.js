6c3866489d64c345996a1d6c840b627f360436ee
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-08 07:02:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Route{
   this.endingLocation=endingLocation
   }
   blocksTravelled(){
-    const 
+    const numofblocktravelled = 
     return 
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
   }
