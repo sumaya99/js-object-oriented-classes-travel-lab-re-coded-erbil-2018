fe7376effc115ac821e3e39c6171f69c65d1d97f
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-08 06:58:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,8 +20,12 @@ class Route{
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
   }
   
-  estimatedTime(){
-    
-    
+  estimatedTime(min){
+    console.log
+    if(min === blocksTravelled){
+    return undefined;}
+    else{
+      return
+    }
   }
 }
