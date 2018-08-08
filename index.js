f4f3868f22bd82660f819d0456903ff0cc57f1ca
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-08 07:20:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+class Driver{
+  constructor(name, startDate ){
+  this.name=name
+  this.startDate=new Date(startDate)
+  }
+  yearsExperienceFromBeginningOf(year){
+    return year-this.startDate.getFullYear();
+  }
+}
+let driver = new Driver("Alfie", "Aug 9, 1995")
+driver.yearsExperienceFromBeginningOf(2018);
 
+class Route{
+  constructor(beginningLocation, endingLocation ){
+  this.beginningLocation=beginningLocation;
+  this.endingLocation=endingLocation;
+  }
+  blocksTravelled(){
+    const numofblocktravelled = Math.abs(parseInt
+    (this.beginningLocation.vertical)-parseInt
+    (this.endingLocation.vertical));
+    
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    
+        const avenue = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+        return numofblocktravelled + avenue;
+  }
+  
+  estimatedTime(min){
+    console.log(this.blocksTravelled())
+    if(min === undefined){
+    return this.blocksTravelled()/3;}
+    else{
+      return this.blocksTravelled()/2;
+    }
+  }
+}
+
+
+let route = new Route({horizontal: '1st Avenue', vertical: '34'}, {horizontal: 'Park', vertical: '45'})
+route.blocksTravelled()
+route.estimatedTime()
