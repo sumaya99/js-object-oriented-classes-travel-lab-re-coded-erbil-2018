74ecb4f625d6e2ddd3d7edd0c9df00aa905fba3e
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-08 07:12:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ class Route{
   estimatedTime(min){
     console.log(this.blocksTravelled())
     if(min === undefined){
-    return this.blocksTravelled()2;}
+    return this.blocksTravelled()/2;}
     else{
       return this.blocksTravelled()/3;
     }
