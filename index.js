c6c8dceb022edd217f294b18c61dfd3bb0a2cadb
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-08 06:25:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class Driver{
   constructor(name, thedayjoined )
   this.name=name
   this.thedayjoined=thedayjoined
-  
+  startDate
   yearsExperienceFromBeginningOf
 }
 driver = new Driver("Alfie", "Aug 9, 1995")
