67d0ddac6785bf918785b32db8b993fa615c3bed
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-08 07:17:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ class Route{
     const numofblocktravelled = Math.abs(parseInt
     (this.beginningLocation.vertical)-parseInt
     (this.endingLocation.vertical));
+    
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    
         const avenue = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal)-eastwest.indexOf(this.endingLocation.horizontal));
         return numofblocktravelled + avenue;
   }
