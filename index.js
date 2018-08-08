c7b0ee61760f4514983f754002e789c204ede3cd
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-08 06:42:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class Route extends Driver{
   this.endingLocation=endingLocation
   }
   Driver.prototype.blocksTravelled(){}
-  Driver.prototype.yearsExperienceFromBeginningOf(){}
+  Driver.prototype.estimatedTime(){}
 }
 let driver = new Driver("Alfie", "Aug 9, 1995")
 let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
